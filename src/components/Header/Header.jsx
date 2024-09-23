8b66b3b7c387880aefc8cd3d893e0feef6cfe358
kananguluzade <guluzdehkanan@gmail.com>
2024-09-23 17:22:31 +0400

    last version hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import  React, { useState } from "react";
+import React, { useState } from "react";
 import logo from "../../../public/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -28,44 +28,46 @@ const Header = () => {
   ];
 
   return (
-    <div className="container">
-      <div className={styles.header}>
-        <div className={styles.header__logo}>
-          <img src={logo} alt="Second Life Logo" />
-        </div>
-        <div className={styles.hamburger} onClick={toggleMenu}>
-          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
-        </div>
-        <div className={styles.header__categories}>
-          <div className={styles.dropdown}>
-            <label
-              className={`${styles.dropdown__label} ${
-                isOpen ? styles.active : ""
-              }`}
-              onClick={toggleDropdown}
-            >
-              Kateqoriyalar <FontAwesomeIcon icon={faAngleDown} />
-            </label>
-            <ul
-              className={`${styles.dropdown__menu} ${
-                isOpen ? styles.show : ""
-              }`}
-            >
-              {categories.map((item, index) => (
-                <li key={index} className={styles.dropdown__item}>
-                  {item.label}
-                </li>
-              ))}
-            </ul>
+    <div className="header-border">
+      <div className="container">
+        <div className={styles.header}>
+          <div className={styles.header__logo}>
+            <img src={logo} alt="Second Life Logo" />
           </div>
-        </div>
-        <div className={styles.header__search}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
-          <input type="text" name="search" placeholder="Axtarış..." />
-        </div>
-        <div className={styles.header__buttons}>
-          <button className={styles.button__register}>Qeydiyyat</button>
-          <button className={styles.button__login}>Giriş</button>
+          <div className={styles.hamburger} onClick={toggleMenu}>
+            <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+          </div>
+          <div className={styles.header__categories}>
+            <div className={styles.dropdown}>
+              <label
+                className={`${styles.dropdown__label} ${
+                  isOpen ? styles.active : ""
+                }`}
+                onClick={toggleDropdown}
+              >
+                Kateqoriyalar <FontAwesomeIcon icon={faAngleDown} />
+              </label>
+              <ul
+                className={`${styles.dropdown__menu} ${
+                  isOpen ? styles.show : ""
+                }`}
+              >
+                {categories.map((item, index) => (
+                  <li key={index} className={styles.dropdown__item}>
+                    {item.label}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className={styles.header__search}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <input type="text" name="search" placeholder="Axtarış..." />
+          </div>
+          <div className={styles.header__buttons}>
+            <button className={styles.button__register}>Qeydiyyat</button>
+            <button className={styles.button__login}>Giriş</button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { Carousel } from "antd";
 import firstHeroImage from "../../../public/first-hero.jpg";
+import secondHeroImage from "../../../public/second-hero.jpg";
+import thirdHeroImage from "../../../public/third-hero.jpg";
 import styles from "../Home/Home.module.css";
-
-const contentStyle = {
-  margin: 0,
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-};
 
 const Home = () => {
   return (
@@ -30,10 +23,30 @@ const Home = () => {
           </div>
         </div>
         <div className={styles.heroPage}>
-          <h3 style={contentStyle}>2</h3>
+          <div className={styles.heroLeftContent}>
+            <h3 className={styles.mainContent}>
+              Sonsuz imkanları kəşf et və dəyərlərini RentAll ilə müəyyənləşdir!
+            </h3>
+            <p className={styles.helperContent}>
+              Artıq lazım olmayanı sat və ona yeni sahib tap!
+            </p>
+          </div>
+          <div className={styles.heroRightContent}>
+            <img src={secondHeroImage} alt="First Hero" />
+          </div>
         </div>
         <div className={styles.heroPage}>
-          <h3 style={contentStyle}>3</h3>
+          <div className={styles.heroLeftContent}>
+            <h3 className={styles.mainContent}>
+              Sonsuz imkanları kəşf et və dəyərlərini RentAll ilə müəyyənləşdir!
+            </h3>
+            <p className={styles.helperContent}>
+              Artıq lazım olmayanı sat və ona yeni sahib tap!
+            </p>
+          </div>
+          <div className={styles.heroRightContent}>
+            <img src={thirdHeroImage} alt="First Hero" />
+          </div>
         </div>
       </Carousel>
     </div>
